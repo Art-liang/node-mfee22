f5ed8fc530e44cf69ffb5d6d1673412efad0bc8d
ALICE LIANG <81024991+Art-liang@users.noreply.github.com>
2021-12-26 14:19:58 +0800

    addsum

---
+++ basic/sum.js
@@ -0,0 +1,12 @@
+//sum.js
+
+function sum(n) {
+// TOD0:
+// result = 1 + 2 + 3 + .... + n
+return XPathResult;
+}
+
+console.log(sum(1)); // 1
+console.log(sum(2)); //3
+console.log(sum(5)); //15
+console.log(sum(10)); //55
