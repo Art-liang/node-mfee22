8d091efeebe596bf10af85cec29e1c0059ea6226
alice <a0975012712@gmail.com>
2022-01-23 10:49:20 +0800

    update2

--- basic/hello.js
+++ basic/hello.js
@@ -1,9 +1,18 @@
-let name = "小梁";
+console.log("start");
 
-console.log(`Hello Node from ${name}`);
+setTimeout(function () {
+  console.log("Timeout1");
+  // 要花很久時間的事
+  for (let i = 0; i < 100000; i++) {
+    let result = 0;
+    for (let j = 0; j < 100000; j++) {
+      result += j;
+    }
+  }
+}, 0);
 
-console.log ("start");
-SetTimeout(function () {
-    console.log("Timeout");
-}, 1000);
+setTimeout(function () {
+  console.log("Timeout2");
+}, 0);
+
 console.log("end");
